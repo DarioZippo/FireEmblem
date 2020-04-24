a2899a1834d7d3ef71df6f3d7f5ab23efbe08696
Dario Zippo <dario_zippo@hotmail.it>
2020-04-24 18:56:54 +0200

    Rimossi i commenti di debug su Automove

--- js/enemy.js
+++ js/enemy.js
@@ -5,8 +5,9 @@ function()
     var that = this;
     setTimeout(function(){that.autoMove();}, 2000);
     setTimeout(function(){updateBoardCharacters();}, 3000);
-    setTimeout(function(){cleanArea();}, 4000);
-    /*this.autoAttack();*/
+    setTimeout(function(){cleanArea();}, 3500);
+    setTimeout(function(){that.showAttackArea();}, 4000);
+    setTimeout(function(){that.autoAttack();}, 4500);
 }
 
 Character.prototype.autoMove =
@@ -14,31 +15,22 @@ function()
 {
     var startBlock = board.blocks[this.x * len + this.y];
     var currentBlock, minDistanceBlock;
-    var str;
     for(var i = 0; i < playerTeam.length; i++)
     {
-        //alert(playerTeam[i].name);
-        if(i == 0)
+        if(playerTeam[i].alive == true)
         {
-            minDistanceBlock = checkMinDistance(this, playerTeam[i]);
+            if(i == 0)
+            {
+                minDistanceBlock = checkMinDistance(this, playerTeam[i]);
+            }
+            else
+            {
+                currentBlock = checkMinDistance(this, playerTeam[i]);
+                if(currentBlock.dist < minDistanceBlock.dist)
+                    minDistanceBlock = currentBlock;
+            }
         }
-        else
-        {
-            currentBlock = checkMinDistance(this, playerTeam[i]);
-            //str = "Current " + i +": "+ currentBlock.block.x +", " + currentBlock.block.y + " -> " + currentBlock.dist;
-            //alert(str);
-            if(currentBlock.dist < minDistanceBlock.dist)
-                minDistanceBlock = currentBlock;
-        }
-        //str = "Min " + i +": "+ minDistanceBlock.block.x +", " + minDistanceBlock.block.y + " -> " + minDistanceBlock.dist;
-        //alert(str);
     }
-    var start = startBlock.x + ", " + startBlock.y;
-    var end = minDistanceBlock.block.x + ", " + minDistanceBlock.block.y;
-    /*
-        alert(start);
-        alert(end);
-    */
     this.move(minDistanceBlock.block);
 }
 
@@ -70,29 +62,17 @@ function checkMinDistance(character1, character2)
             {
                 currentBlock.block = character1.area.movementMatrix[i];
                 currentBlock.dist = currentDistanceX + currentDistanceY; 
-                /*
-                    str = "Current " + i +": "+ currentBlock.block.x +", " + currentBlock.block.y + " -> " + currentBlock.dist;
-                    alert(str);
-                    str = "Min before " + i +": "+ minDistanceBlock.block.x +", " + minDistanceBlock.block.y + " -> " + minDistanceBlock.dist + " K = " + k;
-                    alert(str);
-                */
+
                 if(currentBlock.dist < minDistanceBlock.dist)
                 {
                     minDistanceBlock.block = currentBlock.block;
                     minDistanceBlock.dist = currentBlock.dist;
                 }
             }
+            
             k++;
-            /*
-                str = "Min after " + i +": "+ minDistanceBlock.block.x +", " + minDistanceBlock.block.y + " -> " + minDistanceBlock.dist + " K = " + k;
-                alert(str);
-            */
         }
     }
-    /*
-        alert(minDistanceBlock.dist);
-        alert(minDistanceBlock.block.x);
-        alert(minDistanceBlock.block.y);
-    */
+
     return minDistanceBlock;
 }
