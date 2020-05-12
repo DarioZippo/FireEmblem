1ad4508422b38b3e5f6f0d6ca793b525b7f235c0
Dario Zippo <dario_zippo@hotmail.it>
2020-05-12 14:47:21 +0200

    Query showShop

--- php/util/sessionUtil.php
+++ php/util/sessionUtil.php
@@ -114,4 +114,18 @@
 
 		return $count;
 	}
+
+	function showShop()
+	{
+		global $FireEmblemDB;
+
+		$query = "SELECT * "
+				."FROM item "
+				."ORDER BY type";
+
+		$result = $FireEmblemDB->performQuery($query);
+		$FireEmblemDB->CloseConnection();
+		return $result;
+	}
 ?>
+
